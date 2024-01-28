e586d9b3e5d0cdb609bedb3ae14ae40f81d4276e
Suiiinaldo <toprakhar03@gmail.com>
2024-01-29 03:46:39 +0530

    Added middlewares for signin and signup

---
+++ src/middlewares/auth-request-middlewares.js
@@ -0,0 +1,56 @@
+const { StatusCodes } = require("http-status-codes");
+const { ErrorResponse } = require("../utils/common");
+const AppError = require("../utils/errors/app-error");
+
+function validateAuthRequestSignIn(req,res,next){
+    if(!req.body.emailOrUsername){
+        ErrorResponse.message = "Something went wrong while authenticating";
+        ErrorResponse.error = new AppError([ "Email or usernmae not found in the incoming request" ],StatusCodes.BAD_REQUEST);
+        return res
+                .status(StatusCodes.BAD_REQUEST)
+                .json(ErrorResponse);
+
+    }
+    if(!req.body.password){
+        ErrorResponse.message = "Something went wrong while authenticating";
+        ErrorResponse.error = new AppError([ "Password not found in the incoming request in the correct form" ],StatusCodes.BAD_REQUEST);
+        return res
+                .status(StatusCodes.BAD_REQUEST)
+                .json(ErrorResponse);
+
+    }
+    next();
+}
+
+function validateAuthRequestSignUp(req,res,next){
+    if(!req.body.email){
+        ErrorResponse.message = "Something went wrong while authenticating";
+        ErrorResponse.error = new AppError([ "Email not found in the incoming request" ],StatusCodes.BAD_REQUEST);
+        return res
+                .status(StatusCodes.BAD_REQUEST)
+                .json(ErrorResponse);
+
+    }
+    if(!req.body.password){
+        ErrorResponse.message = "Something went wrong while authenticating";
+        ErrorResponse.error = new AppError([ "Password not found in the incoming request in the correct form" ],StatusCodes.BAD_REQUEST);
+        return res
+                .status(StatusCodes.BAD_REQUEST)
+                .json(ErrorResponse);
+
+    }
+    if(!req.body.username){
+        ErrorResponse.message = "Something went wrong while authenticating";
+        ErrorResponse.error = new AppError([ "Username not found in the incoming request in the correct form" ],StatusCodes.BAD_REQUEST);
+        return res
+                .status(StatusCodes.BAD_REQUEST)
+                .json(ErrorResponse);
+
+    }
+    next();
+}
+
+module.exports = {
+    validateAuthRequestSignIn,
+    validateAuthRequestSignUp
+};
--- src/routes/v1/user-routes.js
+++ src/routes/v1/user-routes.js
@@ -2,13 +2,16 @@ const express = require("express");
 
 const router = express.Router();
 
-const { UserController } = require("../../controller")
+const { UserController } = require("../../controller");
+const { AuthRequestMiddlewares } = require("../../middlewares");
 
 
 router.post("/signup",
+            AuthRequestMiddlewares.validateAuthRequestSignUp,
             UserController.signup);
 
 router.post("/signin",
+            AuthRequestMiddlewares.validateAuthRequestSignIn,
             UserController.signin);      
 
 module.exports = router;
