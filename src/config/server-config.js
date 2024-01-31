2de4f7894a86c49596c259fdde86094bc3a76a7e
Suiiinaldo <toprakhar03@gmail.com>
2024-01-31 22:14:22 +0530

    Added rate limiter and proxy

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -7,4 +7,6 @@ module.exports = {
   SALT_ROUNDS: process.env.SALT_ROUNDS,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY,
+  FLIGHTS_URL: process.env.FLIGHTS_URL,
+  BOOKING_URL: process.env.BOOKING_URL,
 };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,41 @@
 const express = require("express");
-
+const rateLimit  = require('express-rate-limit');
 const { ServerConfig, Logger } = require("./config");
+
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const apiRoutes = require("./routes");
 
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 2 * 60 * 1000, // 2 minutes
+	max: 15, // Limit each IP to 5 requests per `window`.
+})
+
+
+app.use('/flightsService', createProxyMiddleware({ 
+    target: ServerConfig.FLIGHTS_URL, 
+    changeOrigin: true,
+    pathRewrite: { 
+      '^/flightsService' : '/'
+    }
+}));
+
+
+app.use('/bookingService', createProxyMiddleware({ 
+    target: ServerConfig.BOOKING_URL, 
+    changeOrigin: true,
+    pathRewrite: { 
+      '^/bookingService' : '/'
+    }
+}));
+
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use(limiter);
 
 app.use("/api", apiRoutes);
 
